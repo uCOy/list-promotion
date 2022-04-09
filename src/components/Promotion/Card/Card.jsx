111e17f7d30280781ce9a8d76faf20c74127a82a
RICHARD HENRIQUE COUTO DA ROCHA <richard.hcrocha@senacsp.edu.br>
2022-04-08 22:09:25 -0300

    08/04/22

--- src/components/Promotion/Card/Card.jsx
+++ src/components/Promotion/Card/Card.jsx
@@ -4,24 +4,30 @@ import card from './card.module.css';
 function PromotionCard({promotion}){
 
     return (
-        <div className={card.promotion_card}>
-            <img className={card.promotion_card_image} src={promotion.imageUrl} alt={promotion.title} />
+        <div className={card.promotionCard}>
+            <img className={card.promotionCardImage} src={promotion.imageUrl} alt={promotion.title} />
             <div>
-                <h1>{promotion.title}</h1>
-                <span>$ {promotion.price}</span>
-                <footer>
-                    <div>
+                <h1 className={card.promotionCardTitle}>{promotion.title}</h1>
+                <span className={card.promotionCardPrice}>$ {promotion.price}</span>
+                <footer className={card.promotionCardFooter}>
+                    <div className={card.promotionCardComment}>
                         {promotion.comments.length > 0 && (
                             <div>
                                 "{promotion.comments[0].comment}"
                             </div>
                         )}
                     </div> 
-                    <div>
+                    <div className={card.promotionCardCommentsCount}>
                         {promotion.comments.length}{' '}
                         {promotion.comments.length > 1 ? 'Comentários' : 'Comentário'}
                     </div>
-                    <a href={promotion.url} target="_blank">IR PARA O SITE</a>
+                    <a 
+                    href={promotion.url}
+                    className={card.promotionCardLink} 
+                    target="_blank"
+                    >
+                        IR PARA O SITE
+                    </a>
                 </footer>
             </div>
         </div>
